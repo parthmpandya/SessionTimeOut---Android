a48ba861d1ac499a7f9ee7835c28a60b8ce3c843
Parth Pandya <parth@net4nuts.com>
2015-11-17 16:55:36 +0530

    Session Time Out - Android

---
+++ src/session/time/out/SecondActivity.java
@@ -0,0 +1,12 @@
+package session.time.out;
+
+import android.os.Bundle;
+
+public class SecondActivity extends MyBaseActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_second);
+	}
+}
